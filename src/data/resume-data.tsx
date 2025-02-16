97bb0c6a2f30747669cd81763b61637d53300cc8
pawelkomarnickihrme <124041781+pawelkomarnickihrme@users.noreply.github.com>
2025-02-16 20:01:53 +0100

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -26,7 +26,7 @@ export const RESUME_DATA = {
   locationLink: "https://www.google.com/maps/place/Olsztyn",
   about: "Full Stack Engineer",
   summary:
-    "Experienced Full Stack Developer with 5 years of expertise in crafting robust and scalable web applications. Proficient in JavaScript, TypeScript, React, and Node.js, I bring a comprehensive skill set to both front-end and back-end development. I hold a Bachelor's degree in Computer Science, which has provided me with a solid foundation in software engineering principles. My passion for learning drives me to constantly explore and adapt to new technologies, ensuring I stay at the forefront of the ever-evolving tech landscape. ",
+    "Senior Full Stack Developer with 5 years of expertise in building scalable web applications. I run my own business and collaborate on a B2B basis in Olsztyn or remotely. Proficient in JavaScript, TypeScript, React, and Node.js, with a strong foundation in software engineering principles from a Bachelor's degree in Computer Science.",
   avatarUrl:
     "https://wvedlmseeecpqd04.public.blob.vercel-storage.com/profil%20cut.png",
   // personalWebsiteUrl: "https://jarocki.me",
@@ -68,7 +68,7 @@ export const RESUME_DATA = {
       start: "2023",
       end: "Now",
       description:
-        "Developed new modules and maintained existing ones in Next.js and TypeScript, directly managing the development of spidersweb.pl, rozrywka.spidersweb.pl, bizblog.spidersweb.pl, and autoblog.spidersweb.pl. Additionally, contributed indirectly to projects for prominent brands such as Samsung, Chanel, Amazon, and Allegro. Took responsibility for the entire development process, from initial project design to production deployment.",
+        "Senior Full Stack Developer specializing in Next.js and TypeScript, responsible for the entire frontend of high-traffic websites such as www.spidersweb.pl, www.rozrywka.spidersweb.pl, www.bizblog.spidersweb.pl, and www.autoblog.spidersweb.pl.",
     },
     {
       company: "Mikronika",
@@ -82,7 +82,7 @@ export const RESUME_DATA = {
     },
     {
       company: "Hr Me",
-      link: "https://beta.hr-me.co/",
+      link: "https://hr-me.co/",
       badges: ["Remote", "Side Project"],
       title: "Frontend  Developer",
       start: "2022",
@@ -95,7 +95,7 @@ export const RESUME_DATA = {
     "JavaScript",
     "TypeScript",
     "React",
-    "Next.js",
+    "Next",
     "Redux",
     "React Context",
     "Tailwind CSS",
@@ -110,7 +110,6 @@ export const RESUME_DATA = {
     "Postgres",
     "Firebase",
     "NestJS",
-    "tRPC",
     "Docker",
     "Vercel",
     "Git",
@@ -136,166 +135,5 @@ export const RESUME_DATA = {
     "English B2 level",
   ],
 
-  projects: [
-    {
-      title: "Consultly",
-      techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-      ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
-  ],
+  projects: [],
 } as const;
